fc651a68e8db2d33ab1afdf762c28145d2db84f3
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-09-13 08:08:53 -0300

    feat: implements access to fetch API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,32 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const END_POINT = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const response = await fetch(END_POINT);
+    const categories = await response.json();
+    return categories;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  try {
+    const END_POINT = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    const response = await fetch(END_POINT);
+    const productsList = await response.json();
+    return productsList;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getProductById(productId) {
+  try {
+    const END_POINT = `https://api.mercadolibre.com/items/${productId}`;
+    const response = await fetch(END_POINT);
+    const productData = await response.json();
+    return productData;
+  } catch (err) {
+    console.error(err);
+  }
 }
