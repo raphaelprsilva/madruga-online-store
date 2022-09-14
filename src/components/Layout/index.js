18de62a39383cc2dde500d25406d341c044f8a60
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-09-14 08:52:51 -0300

    feat: add Header component to Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Header from '../Header';
 
 class Layout extends Component {
   render() {
     const { children } = this.props;
     return (
       <section>
-        <header>Componente Header aqui</header>
+        <Header />
         <main>{children}</main>
       </section>
     );
